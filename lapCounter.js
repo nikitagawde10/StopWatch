90e0c9bf62090622dd5ecd344db8d4ddf9f9427c
Nikita Gawde <nikita.gawde@weareadaptive.com>
2021-06-20 22:43:16 -0400

    fixing css

--- lapCounter.js
+++ lapCounter.js
@@ -1,5 +1,28 @@
-export default function lapCount(passedTimeInMilliseconds){
-    const laps = document.getElementsByClassName("laps")[0];
-    
+import getFormattedTime from "./timeCalculator";
 
+export default function lapCount(passedTimeInMilliseconds,numberOfLaps){
+    const laps = document.getElementsByClassName("laps")[0];
+    let lapTimeStamp = Date.now();
+    console.log("Lap time stamp is " + lapTimeStamp);
+    let lapArray = [];
+    if(!lapArray.length){
+        timeStamp.innerHTML = getFormattedTime((lapTimeStamp - passedTimeInMilliseconds));
+    }
+    else{
+        let previousLap = lapArray[lapArray.length - 1];
+        laps.innerHTML = getFormattedTime((lapTimeStamp - previousLap[1]));
+    }
+    lapArray.push([newLap, lapTimeStamp]);
+    console.log('Testing function lap');
+    const li = document.createElement("li");
+    const number = document.createElement("span");
+    const timeStamp = document.createElement("span");
+    li.setAttribute("class", "li-item");
+    number.setAttribute("class", "number");
+    timeStamp.setAttribute("class", "timeStamp");
+    number.innerText = `${++numberOfLaps}`;
+    timeStamp.innerHTML = getFormattedTime((lapTimeStamp - passedTimeInMilliseconds));
+    // timeStamp.innerHTML = getFormattedTime(passedTime);
+    li.append(number, timeStamp);
+    laps.append(li);
 }
--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-import lapCount from './lapCounter.js';
 import getFormattedTime from './timeCalculator.js';
-// import lapCount from './lapCounter.js';
 let startTime;
 let passedTime = 0;
 let timerInterval;
-// let currentMinimumTime = 1000000000000;
 let startButton = document.getElementById("startButton");
 let pauseButton = document.getElementById("pauseButton");
-let resetButton = document.getElementById("resetButton");
-// let lapButton = document.getElementById("lapButton");
+let lapButton = document.getElementById("lapButton");
 let numOfLaps = 0;
+const laps = document.getElementsByClassName("laps")[0];
 
 function print(text) {
     document.getElementById("displayTimerDiv").innerHTML = text;
@@ -21,14 +18,6 @@ function showStartPauseButton(toggleValue) {
     buttonToShow.style.display = "block";
     buttonToHide.style.display = "none";
 }
-// function showResetLapButton(toggleValue) {
-//     const buttonToShow = toggleValue === "LAP" ? lapButton : resetButton;
-//     const buttonToHide = toggleValue === "LAP" ? resetButton : lapButton;
-//     buttonToShow.style.display = "block";
-//     buttonToHide.style.display = "none";
-// }
-//research adding css to template literals for max min
-
 startButton.onclick = () => {
     startTime = Date.now() - passedTime;
     timerInterval = setInterval(function printTime() {
@@ -49,40 +38,45 @@ resetButton.onclick = () => {
     clearInterval(timerInterval);
     print("00 : 00 : 00");
     passedTime = 0;
-    // lapCounter = 0;
-    // laps.innerHTML = '';
+    numOfLaps = 0;
+    laps.innerHTML = '';
     // laps.append(resetButton);
     showStartPauseButton("PLAY");
+    // btn.innerHTML = "LAPS";
 }
 
 // lapButton.onclick = () => {
-//     clearInterval(timerInterval);
-    //    numOfLaps++
-    //    lapCounter(numOfLaps);
-//             if(passedTime < currentMinimumTime){
-//                 currentMinimumTime = passedTime;
-//                // show currentMinimumTime in green color print(timeToString(currentMinimumTime));
-//                 passedTime = 0;
-//             }
-//             else if(passedTime > currentMaximumTime){
-//                 currentMaximumTime = passedTime;
-//                 // show currentMaximumTime in green color print(timeToString(currentMaximumTime));
-//                 passedTime = 0;
-//             //    ******** startTimer() should begin here *********
-//             }
-//             else{
-//                 print(timeToString(passedTime));
-//                 passedTime = 0;
-//             }
-//     console.log('Testing function lap');
-//     const li = document.createElement("li");
-//     const number = document.createElement("span");
-//     const timeStamp = document.createElement("span");
-//     li.setAttribute("class", "li-item");
-//     number.setAttribute("class", "number");
-//     timeStamp.setAttribute("class", "timeStamp");
-//     number.innerText = `${++lapCounter}`;
-//     timeStamp.innerHTML = getFormattedTime(passedTime);
-//     li.append(number, timeStamp);
-//     laps.append(li);
-// }
+//     numOfLaps++;
+//     lapCount(passedTime, numOfLaps);
+//     // showLapResetButton("LAP")// }
+lapButton.onclick = () => {
+    // clearInterval(timerInterval);
+//    numOfLaps++
+//    lapCounter(numOfLaps);
+            // if(passedTime < currentMinimumTime){
+            //     currentMinimumTime = passedTime;
+            //    // show currentMinimumTime in green color print(timeToString(currentMinimumTime));
+            //     passedTime = 0;
+            // }
+            // else if(passedTime > currentMaximumTime){
+            //     currentMaximumTime = passedTime;
+            //     // show currentMaximumTime in green color print(timeToString(currentMaximumTime));
+            //     passedTime = 0;
+            // //    ******** startTimer() should begin here *********
+            // }
+            // else{
+                print(getFormattedTime(passedTime));
+                // passedTime = 0;
+            // }
+    console.log('Testing function lap');
+    const li = document.createElement("li");
+    const number = document.createElement("span");
+    const timeStamp = document.createElement("span");
+    li.setAttribute("class", "li-item");
+    number.setAttribute("class", "number");
+    timeStamp.setAttribute("class", "timeStamp");
+    number.innerText = `${++numOfLaps}`;
+    timeStamp.innerHTML = getFormattedTime(passedTime);
+    li.append(number, timeStamp);
+    laps.append(li);
+}
